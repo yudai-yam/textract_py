a1525bcf1fd6bf9ef28c32cd921102d015a24d23
Yudai <yudaimadeinjapan@gmail.com>
2023-08-29 12:02:03 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import datetime
+import json
+import boto3
+from textract import TextractWrapper
+# Importing the PIL library
+from io import BytesIO
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import io
+
+s3_client = boto3.client('s3')
+textract_client = boto3.client('textract')
+
+
+def translate_with_gpt(original_text):
+    # translate text
+    translated_text = "translated"
+    return translated_text
+
+def geometry_analyzer(textract_response):
+    line_blocks = [block for block in textract_response['Blocks'] if block['BlockType'] == 'LINE']
+    
+    line_info_list = []
+    for block in line_blocks:
+        text = block['Text']
+        bounding_box = block['Geometry']['BoundingBox']
+        
+        line_info = {'Text': text, 'BoundingBox': bounding_box}
+        line_info_list.append(line_info)
+    
+    return line_info_list
+
+
+def image_editer(file_byte_string, translated_text_info_list):
+    # Open an Image
+    img = Image.open(BytesIO(file_byte_string))
+
+    # get sizes
+    width, height = img.size
+    
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+
+    for info in translated_text_info_list:
+        # get coordinates from text_info_list
+        x = info['BoundingBox']['Left']
+        y = info['BoundingBox']['Height']
+
+        # Custom font style and font size
+        myFont = ImageFont.truetype('fonts/Helvetica.ttf', 55)
+        
+        # Add Text to an image
+        I1.text((width*x, height*y), "translated", font=myFont, fill=(255, 0, 0))
+    
+    # Display edited image
+    #img.show()
+    
+    # Save the edited image
+    #img.save("images/lancs2.png")
+
+    # Save the image to an in-memory file
+    in_mem_file = io.BytesIO()
+    img.save(in_mem_file, format=img.format)
+    in_mem_file.seek(0)
+
+
+    return in_mem_file
+
+
+def lambda_handler(event, context):
+    print(event)
+    # get bucket and key
+    original_bucket = event['Records'][0]['s3']['bucket']['name']
+    original_key = event['Records'][0]['s3']['object']['key']
+
+    print(original_bucket)
+    print(original_key)
+    
+    
+    # return json.loads(body.decode('utf-8'))
+
+   
+
+    # textract
+    textract_response = textract_client.analyze_document(
+            Document={'S3Object': {'Bucket': original_bucket, 'Name': original_key}},
+            FeatureTypes=['TABLES'])
+    print(textract_response)
+    text_info_list = geometry_analyzer(textract_response)
+
+    print(*text_info_list)
+
+    # translate the text and update text_info_list
+    for index, info in enumerate(text_info_list):
+        original_text = info['Text']
+        translated_text = translate_with_gpt(original_text)
+        text_info_list[index]['Text'] = translated_text
+    translated_text_info_list = text_info_list
+
+    print(*translated_text_info_list)
+
+    # get image from s3
+    response = s3_client.get_object(Bucket=original_bucket, Key=original_key)
+    body = response['Body'].read()
+
+
+    # pass the image it got from s3 to the image manipulation fucnc
+    edited_img = image_editer(body, translated_text_info_list)
+
+    # save the edited image to another bucket
+    new_bucket = 'textract-result-py'
+    new_key = 'test.png'
+
+    response = s3_client.upload_fileobj(
+        edited_img, # This is what i am trying to upload
+        new_bucket,
+        new_key
+    )
+
+    print(response)
+
+
+    
---
+++ textract.py
@@ -0,0 +1,40 @@
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+class TextractWrapper:
+    """Encapsulates Textract functions."""
+    def __init__(self, textract_client):
+        """
+        :param textract_client: A Boto3 Textract client.
+        :param s3_resource: A Boto3 Amazon S3 resource.
+        :param sqs_resource: A Boto3 Amazon SQS resource.
+        """
+        self.textract_client = textract_client
+    
+    def analyze_file(self, feature_types, *, document_file_name=None, document_bytes=None):
+        """
+        Detects text and additional elements, such as forms or tables, in a local image
+        file or from in-memory byte data.
+        The image must be in PNG or JPG format.
+
+        :param feature_types: The types of additional document features to detect.
+        :param document_file_name: The name of a document image file.
+        :param document_bytes: In-memory byte data of a document image.
+        :return: The response from Amazon Textract, including a list of blocks
+                that describe elements detected in the image.
+        """
+        if document_file_name is not None:
+            with open(document_file_name, 'rb') as document_file:
+                document_bytes = document_file.read()
+        try:
+            response = self.textract_client.analyze_document(
+                Document={'Bytes': document_bytes}, FeatureTypes=feature_types)
+            logger.info(
+                "Detected %s blocks.", len(response['Blocks']))
+        except ClientError:
+            logger.exception("Couldn't detect text.")
+            raise
+        else:
+            return response
