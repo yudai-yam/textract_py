73e722e9c95fe85d9cb4aee136e46cdf7ac5bf12
Yudai <yudaimadeinjapan@gmail.com>
2023-09-23 15:52:22 +0900

    multiple line translation in one go, but Japanese not available, special case like texts next to each other still does not work

--- main.py
+++ main.py
@@ -19,19 +19,91 @@ textract_client = boto3.client('textract')
 openai.api_key = constants.API_KEY
 
 
-def translate_with_gpt(original_text):
+current_block_info_storage = [] # store the current block info that are in the same chunk, this is used only in the block_relation_checker
+txt_line_storage = [] # store the full text and its number of LINEs, this is used throughout the program
+
+
+# identify text block
+def block_relation_checker(anchor_x, anchor_y, block_height, text, last):
+    global current_block_info_storage
+    global txt_line_storage
+
+    print("the text being examined is "+text)
+    
+
+    if (current_block_info_storage == []): # the very first line
+        block_info = [anchor_x, anchor_y, block_height, text]
+        current_block_info_storage.append(block_info)
+        print("it's the very first sentence")
+    
+    else: # not the very first line
+        # check the space in between two texts (blocks)
+        print("this is not the first sentence")
+        prev_anchor_y = current_block_info_storage[-1][1]
+        prev_block_height = current_block_info_storage[-1][2]
+        space = anchor_y - (prev_anchor_y + prev_block_height) # represented by ratio against the original img size
+
+        if (space > block_height):
+            print("space>block")
+            # belongs to another block
+            # combine existing texts for translation
+            combined_text = ' '.join(block_info[3] for block_info in current_block_info_storage)
+            # store the number of LINE in a single block
+            num_of_line = len(current_block_info_storage)
+
+            txt_line_storage.append([combined_text, num_of_line])
+            print("the text being appended is" + combined_text)
+            # start from scratch
+            current_block_info_storage = []
+
+            if (last == True):
+                txt_line_storage.append([text, 1])
+                print("last element detected")
+
+            block_info = [anchor_x, anchor_y, block_height, text]
+            current_block_info_storage.append(block_info)   
+
+        elif (space <= block_height):
+            block_info = [anchor_x, anchor_y, block_height, text]
+            current_block_info_storage.append(block_info)  
+
+            if (last == True):
+                print("last element detected")
+
+                # combine existing texts for translation
+                combined_text = ' '.join(block_info[3] for block_info in current_block_info_storage)
+                # store the number of LINE in a single block
+                num_of_line = len(current_block_info_storage)
+
+                txt_line_storage.append([combined_text, num_of_line])
+
+
+    print("the current block info storage is")
+    print(*current_block_info_storage)
+    print("the txt_line_storage is")
+    print(*txt_line_storage)
+
+
+
+    #TODO check if it's in the same parallel line
+
+
+
+def translate_with_gpt(original_texts):
     # Define the system message
     system_msg = 'You are a helpful tranlator.'
 
     # Define the user message
-    user_msg = 'Simply translate this text into Spainsh. The text is: '+ original_text
+    #user_msg = f'''Simply translate this text into Spainsh. The text is: '
+                #+ {original_text}
+                #+ '.\nIf this text is incomplete, just return "INCMP" instead of translating. Note that just a noun is considered complete, even without an article.'''
+    
+    user_msg = "Simply translate these texts into Spanish. Each translation result has to start with ##. The sentences to be translated are" + original_texts
     # Create a dataset using GPT
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[{"role": "system", "content": system_msg},
                                             {"role": "user", "content": user_msg}])
     translated_text = response["choices"][0]["message"]["content"]
-
-    print(translated_text)
 
     return translated_text
 
@@ -69,7 +141,7 @@ def image_editer(file_byte_string, translated_text_info_list):
 
         # add rectangles on top of the original text
         I1.rectangle(
-            [(img_width*anchor_x - txt_width*img_width*0.1, img_height*anchor_y), ((img_width*anchor_x + txt_width*img_width + txt_width*img_width*0.1), (img_height*anchor_y + txt_height*img_height))], fill=(255, 255, 255)
+            [(img_width*anchor_x, img_height*anchor_y), ((img_width*anchor_x + txt_width*img_width), (img_height*anchor_y + txt_height*img_height))], fill=(205, 200, 205)
         )
 
 
@@ -107,6 +179,48 @@ def image_editer(file_byte_string, translated_text_info_list):
     return in_mem_file
 
 
+# sentence is a sentence to be splitted
+# ratio is a list containing multiple floating point numbers, the sum is not necessarily 1.0 ex. ratios = [0.4, 0.3, 1.4]
+def split_sentence_by_characters(sentence, char_ratios):
+    # Calculate the total number of characters in the sentence
+    total_chars = len(sentence)
+    
+    # Calculate the split indices based on the normalized char_ratios
+    total_ratio = sum(char_ratios)
+    normalized_ratios = [ratio / total_ratio for ratio in char_ratios]
+    
+    split_indices = []
+    cumulative_ratio = 0
+    for ratio in normalized_ratios[:-1]:
+        split_index = int(total_chars * (cumulative_ratio + ratio))
+        
+        # Make sure not to split in the middle of a word
+        while split_index < total_chars and not sentence[split_index].isspace():
+            split_index += 1
+        
+        split_indices.append(split_index)
+        cumulative_ratio += ratio
+    
+    # Create the parts by iterating through split_indices
+    parts = [] # containing some splitted sentences as a list
+    start_idx = 0
+    for split_idx in split_indices:
+        part = sentence[start_idx:split_idx]
+        parts.append(part.strip())  # Remove leading/trailing spaces
+        start_idx = split_idx
+        print("the part is ")
+        print(*parts)
+    
+    # Add the last part
+    parts.append(sentence[start_idx:])
+    print("the part is ")
+    print(*parts)
+    return parts
+
+
+
+
+
 def lambda_handler(event, context):
     print(event)
     # get bucket and key
@@ -128,13 +242,179 @@ def lambda_handler(event, context):
     print(textract_response)
     text_info_list = geometry_analyzer(textract_response)
 
+    print("the text_info_list is")
     print(*text_info_list)
 
-    # translate the text and update text_info_list
+
+
+
+
+
+
+    # use block relation analysis
     for index, info in enumerate(text_info_list):
+        anchor_x = info['BoundingBox']['Left']
+        anchor_y = info['BoundingBox']['Top']
+        block_height = info['BoundingBox']['Height']
+        text = info['Text']
+        last = False
+        if index+1 == len(text_info_list):
+            last = True
+        block_relation_checker(anchor_x, anchor_y, block_height, text, last)
+
+
+
+
+
+    # format original texts
+    original_texts = ''
+    for index, info in enumerate(txt_line_storage):
+        original_texts += (str(index+1) + ' ')
+        original_texts += info[0]
+        original_texts += '\n'
+    
+    print('the original texts are below')
+    print(original_texts)
+
+
+
+
+
+
+
+
+    # translate them
+    translated_texts = translate_with_gpt(original_texts)
+    print(translated_texts)
+
+    sentences = translated_texts.split("\n##")
+
+
+
+
+
+    # update txt_line_storage
+    for index_outer, info in enumerate(txt_line_storage):
+        for index_inner, sentence in enumerate(sentences):
+            translated_text = sentence
+            if (index_outer == index_inner):
+                if (index_outer == 0):
+                    translated_text = translated_text[2:]
+                txt_line_storage[index_outer][0] = translated_text
+    translated_txt_line_storage = txt_line_storage
+
+    print(*translated_txt_line_storage) # contains translated texts and the number of LINEs
+
+
+    text_info_list_index = 0
+    translated_text_info_list = []
+
+    #TODO allocate the translated texts into each LINE block
+    current_line = 0
+    for chunk in translated_txt_line_storage:
+        translated_complete_txt = chunk[0]
+        num_of_line = chunk[1]
+
+        # Initialize a list to store the selected dictionaries(containing one relational block)
+        selected_dicts = []
+
+        line_ratios = []
+
+        # Loop through the list and select dictionaries within the specified range
+        for position in range(current_line, current_line+num_of_line):
+            if 0 <= position < len(text_info_list):
+                selected_dicts.append(text_info_list[position])
+
+        # Access the 'Width' values of the selected dictionaries using a loop
+        for selected_dict in selected_dicts:
+            width = selected_dict['BoundingBox']['Width']
+            line_ratios.append(width)
+            print("Width:", width)
+
+
+        current_line += num_of_line
+
+        print("the selected dicts is")
+        print(*selected_dicts)
+
+        print("the line ratio is")
+        print(*line_ratios)
+    
+        splitted_result = split_sentence_by_characters(translated_complete_txt, line_ratios)
+        
+
+        for splitted_text in splitted_result:
+            line_info = {'Text': splitted_text, 'BoundingBox': text_info_list[text_info_list_index]['BoundingBox']}
+            translated_text_info_list.append(line_info)
+            text_info_list_index += 1
+
+    
+    print("the translated_text_info_list is")
+    print(*translated_text_info_list)
+
+
+
+#########################################################################################
+
+
+    # get image from s3
+    response = s3_client.get_object(Bucket=original_bucket, Key=original_key)
+    body = response['Body'].read()
+
+
+    # pass the image it got from s3 to the image manipulation fucnc
+    edited_img = image_editer(body, translated_text_info_list)
+
+    # save the edited image to another bucket
+    new_bucket = 'textract-result-py'
+    new_key = 'test.png'
+
+    response = s3_client.upload_fileobj(
+        edited_img, # This is what i am trying to upload
+        new_bucket,
+        new_key
+    )
+
+    print(response)
+
+
+##############################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################################
+    """ for index, info in enumerate(text_info_list):
         original_text = info['Text']
         translated_text = translate_with_gpt(original_text)
-        text_info_list[index]['Text'] = translated_text
+        if translated_text == 'INCMP':
+            # don't translate and combine with the next sentence, based on the anchor_x
+            incomplete_sentence += original_text
+            incomplete_sentence += ' '
+            inc_data_list.append[original_text, info['BoundingBox']['Width']]
+            inc_counter += 1
+            if inc_counter >=2 :
+                incomplete_sentence = inc_data_list[0][0] + ' ' + inc_data_list[1][0]
+                translated_text = translate_with_gpt(incomplete_sentence)
+                # split the text into 2 sentences = get the lengh of each original box (txt)
+                #translated_words = translated_text.split()
+                #word_count = len(translated_words)
+                result_parts = split_sentence(translated_text, [0.2, 0.5])
+                for i, part in enumerate(result_parts):
+                    print(f"Part {i + 1}: {part}")
+
+        else:
+            text_info_list[index]['Text'] = translated_text
+            incomplete_sentence = ''
     translated_text_info_list = text_info_list
 
     print(*translated_text_info_list)
@@ -158,6 +438,4 @@ def lambda_handler(event, context):
     )
 
     print(response)
-
-
-    
+ """
