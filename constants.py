1a57ba5e5a1fa6382bb8f8d0a008d4079bb38940
Yudai <yudaimadeinjapan@gmail.com>
2023-10-10 20:18:27 +0200

    with gpt translation

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
-API_KEY = "sk-4D"
+API_KEY = "sk-yh9OUsnY19mkJkhqorl8AiY"
+DEEPL_KEY = "d79584aff6ef9270:fx"
--- main.py
+++ main.py
@@ -11,22 +11,27 @@ import io
 # Import the os package
 import os
 
+import deepl
+
 # Import the openai package
 import openai
+import time
 
 s3_client = boto3.client('s3')
 textract_client = boto3.client('textract')
 openai.api_key = constants.API_KEY
 
 
-current_block_info_storage = [] # store the current block info that are in the same chunk, this is used only in the block_relation_checker
-txt_line_storage = [] # store the full text and its number of LINEs, this is used throughout the program
+#current_block_info_storage = [] # store the current block info that are in the same chunk, this is used only in the block_relation_checker
+#grouped_txt_info = [] # store the full text and its number of LINEs, this is used throughout the program
 
 
 # identify text block
-def block_relation_checker(anchor_x, anchor_y, block_height, text, last):
+""" def block_relation_checker(anchor_x, anchor_y, block_height, text, last):
     global current_block_info_storage
-    global txt_line_storage
+    global grouped_txt_info
+
+    text_block = []
 
     print("the text being examined is "+text)
     
@@ -51,21 +56,21 @@ def block_relation_checker(anchor_x, anchor_y, block_height, text, last):
             # store the number of LINE in a single block
             num_of_line = len(current_block_info_storage)
 
-            txt_line_storage.append([combined_text, num_of_line])
+            grouped_txt_info.append([combined_text, num_of_line])
             print("the text being appended is" + combined_text)
             # start from scratch
             current_block_info_storage = []
 
             if (last == True):
-                txt_line_storage.append([text, 1])
+                grouped_txt_info.append([text, 1])
                 print("last element detected")
 
             block_info = [anchor_x, anchor_y, block_height, text]
-            current_block_info_storage.append(block_info)   
+            current_block_info_storage.append(block_info)
 
         elif (space <= block_height):
             block_info = [anchor_x, anchor_y, block_height, text]
-            current_block_info_storage.append(block_info)  
+            current_block_info_storage.append(block_info)
 
             if (last == True):
                 print("last element detected")
@@ -75,17 +80,187 @@ def block_relation_checker(anchor_x, anchor_y, block_height, text, last):
                 # store the number of LINE in a single block
                 num_of_line = len(current_block_info_storage)
 
-                txt_line_storage.append([combined_text, num_of_line])
+                grouped_txt_info.append([combined_text, num_of_line])
 
 
     print("the current block info storage is")
     print(*current_block_info_storage)
-    print("the txt_line_storage is")
-    print(*txt_line_storage)
-
+    print("the grouped_txt_info is")
+    print(*grouped_txt_info)
 
 
     #TODO check if it's in the same parallel line
+    
+    # check if the y coordinate is the same (very close) as the previous one
+    prev_anchor_y = current_block_info_storage[-1][1]
+    if (-0.01 < prev_anchor_y - anchor_y < 0.01):
+        print("recursive!")
+        # if it's the same, save the previous records in stack (recursive?)
+        #block_relation_checker(anchor_x, anchor_y, block_height, text, last)
+     """
+    
+
+
+""" # identify text block
+def block_relation_checker(text_info_list):
+    current_block_info_storage = []
+    global grouped_txt_info
+
+    for index, info in enumerate(text_info_list):
+        anchor_x = info['BoundingBox']['Left']
+        anchor_y = info['BoundingBox']['Top']
+        block_height = info['BoundingBox']['Height']
+        text = info['Text']
+        last = False
+        if index + 1 == len(text_info_list):
+            last = True
+
+        print("the text being examined is " + text)
+
+
+        
+
+        if (current_block_info_storage == []):  # the very first line
+            block_info = [anchor_x, anchor_y, block_height, text]
+            current_block_info_storage.append(block_info)
+            print("it's the very first sentence")
+
+        else:  # not the very first line
+
+            # TODO check if it's in the same parallel line
+            # check if the y coordinate is the same (very close) as the previous one
+            prev_anchor_y = current_block_info_storage[-1][1]
+            if (-0.01 < prev_anchor_y - anchor_y < 0.01):
+                print("recursive!")
+                # if it's the same, save the previous records in stack (recursive?)
+                block_relation_checker(text_info_list)
+            
+            
+
+            # check the space in between two texts (blocks)
+            print("this is not the first sentence")
+            prev_anchor_y = current_block_info_storage[-1][1]
+            prev_block_height = current_block_info_storage[-1][2]
+            space = anchor_y - (prev_anchor_y + prev_block_height)  # represented by ratio against the original img size
+
+            if (space > block_height):
+                print("space>block")
+                # belongs to another block
+                # combine existing texts for translation
+                combined_text = ' '.join(block_info[3] for block_info in current_block_info_storage)
+                # store the number of LINE in a single block
+                num_of_line = len(current_block_info_storage)
+
+                grouped_txt_info.append([combined_text, num_of_line])
+                print("the text being appended is" + combined_text)
+                # start from scratch
+                current_block_info_storage = []
+
+                if (last == True):
+                    grouped_txt_info.append([text, 1])
+                    print("last element detected")
+
+                block_info = [anchor_x, anchor_y, block_height, text]
+                current_block_info_storage.append(block_info)
+
+            elif (space <= block_height):
+                block_info = [anchor_x, anchor_y, block_height, text]
+                current_block_info_storage.append(block_info)
+
+                if (last == True):
+                    print("last element detected")
+
+                    # combine existing texts for translation
+                    combined_text = ' '.join(block_info[3] for block_info in current_block_info_storage)
+                    # store the number of LINE in a single block
+                    num_of_line = len(current_block_info_storage)
+
+                    grouped_txt_info.append([combined_text, num_of_line])
+
+        print("the current block info storage is")
+        print(*current_block_info_storage)
+        print("the grouped_txt_info is")
+        print(*grouped_txt_info)
+ """
+
+
+def block_relation_checker(text_info_list): #You need width, height, x, y for each LINE
+    current_block_info_storage = [] # 3D list [x, y, width, height, text, the number of consisted lines]
+    # global grouped_txt_info
+
+    for index, info in enumerate(text_info_list):
+        anchor_x = info['BoundingBox']['Left']
+        anchor_y = info['BoundingBox']['Top']
+        block_height = info['BoundingBox']['Height']
+        block_width = info['BoundingBox']['Width']
+        text = info['Text']
+
+        print("the text being examined is " + text)
+
+
+        # check if there's similar x
+        if (current_block_info_storage == []):  # the very first line
+            block_info = [[anchor_x], [anchor_y], [block_width], [block_height], text, 1]
+            current_block_info_storage.append(block_info)
+            print("it's the very first sentence")
+
+        else:  # not the very first line
+
+            # TODO check if it's in the same parallel line
+            # check if the y coordinate is the same (very close) as the previous one
+            for index, each_info in enumerate(current_block_info_storage):
+                prev_anchor_x = each_info[0][-1]
+                prev_anchor_y = each_info[1][-1]
+                prev_block_width = each_info[2][-1]
+                prev_block_height = each_info[3][-1]
+
+                prev_center_x = prev_anchor_x + prev_block_width/2
+                center_x = anchor_x + block_width/2
+
+
+
+                if (-0.01 < prev_anchor_x - anchor_x < 0.01) or (-0.01 < prev_center_x - center_x < 0.01):
+                    print("same vertical line or the center of the text is very close")
+            
+                    # check the space in between two texts (blocks)
+                    space = anchor_y - (prev_anchor_y + prev_block_height)  # represented by ratio against the original img size
+
+                    """  if (space > block_height):
+                        print("space>block")
+                        # belongs to another block
+
+                        block_info = [anchor_x, anchor_y, block_height, text, 1]
+                        current_block_info_storage.append(block_info)
+                        break """
+
+                    if (space <= block_height):
+                        print('very small space, likely to be in the group')
+                        print("the text to be combined with is"+current_block_info_storage[index][4])
+                        print(anchor_y)
+                        current_block_info_storage[index][0].append(anchor_x)
+                        current_block_info_storage[index][1].append(anchor_y)
+                        current_block_info_storage[index][2].append(block_width)
+                        current_block_info_storage[index][3].append(block_height)
+                        text = ' '+ text
+                        current_block_info_storage[index][4] += text
+                        current_block_info_storage[index][5] += 1
+                        break
+
+                if (index+1 == len(current_block_info_storage)):
+                #if (-0.01 < prev_anchor_y - anchor_y < 0.01):
+                    #print("same horizontal line") 
+                    print("could not find any group")
+                    block_info = [[anchor_x], [anchor_y], [block_width], [block_height], text, 1]
+                    current_block_info_storage.append(block_info)
+                    break
+                
+
+        print("the current block info storage is")
+        print(*current_block_info_storage)
+       # print("the grouped_txt_info is")
+        #print(*grouped_txt_info)
+    return current_block_info_storage
+
 
 
 
@@ -98,7 +273,10 @@ def translate_with_gpt(original_texts):
                 #+ {original_text}
                 #+ '.\nIf this text is incomplete, just return "INCMP" instead of translating. Note that just a noun is considered complete, even without an article.'''
     
-    user_msg = "Simply translate these texts into Spanish. Each translation result has to start with ##. The sentences to be translated are" + original_texts
+    user_msg = """Simply translate these texts into English. 
+                If you cannot translate, just print the original text, don't just skip it. 
+                The number of line needs to match the prompt I give you.
+                The sentences to be translated are""" + original_texts
     # Create a dataset using GPT
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[{"role": "system", "content": system_msg},
@@ -141,7 +319,7 @@ def image_editer(file_byte_string, translated_text_info_list):
 
         # add rectangles on top of the original text
         I1.rectangle(
-            [(img_width*anchor_x, img_height*anchor_y), ((img_width*anchor_x + txt_width*img_width), (img_height*anchor_y + txt_height*img_height))], fill=(205, 200, 205)
+            [(img_width*anchor_x, img_height*anchor_y), ((img_width*anchor_x + txt_width*img_width), (img_height*anchor_y + txt_height*img_height))], fill=(255, 255, 255)
         )
 
 
@@ -150,8 +328,8 @@ def image_editer(file_byte_string, translated_text_info_list):
         translated_txt_len = len(translated_txt)
 
         # the text has to be smaller than the height and width of the box
-        if (math.floor(txt_width*img_width/translated_txt_len*2.5) < math.floor(img_height*txt_height)):
-            font_size = math.floor(txt_width*img_width/translated_txt_len*2.5)  # for some reason calculated font size doesnt match WITHOUT *2.5
+        if (math.floor(txt_width*img_width/translated_txt_len*2 ) < math.floor(img_height*txt_height)):
+            font_size = math.floor(txt_width*img_width/translated_txt_len*2)  # for some reason calculated font size doesnt match WITHOUT *2.5
         else:
             font_size = math.floor(img_height*txt_height)
             print("size got determined based on the height")
@@ -252,15 +430,7 @@ def lambda_handler(event, context):
 
 
     # use block relation analysis
-    for index, info in enumerate(text_info_list):
-        anchor_x = info['BoundingBox']['Left']
-        anchor_y = info['BoundingBox']['Top']
-        block_height = info['BoundingBox']['Height']
-        text = info['Text']
-        last = False
-        if index+1 == len(text_info_list):
-            last = True
-        block_relation_checker(anchor_x, anchor_y, block_height, text, last)
+    grouped_txt_info = block_relation_checker(text_info_list) # contains each geo info and translated texts
 
 
 
@@ -268,9 +438,9 @@ def lambda_handler(event, context):
 
     # format original texts
     original_texts = ''
-    for index, info in enumerate(txt_line_storage):
-        original_texts += (str(index+1) + ' ')
-        original_texts += info[0]
+    for info in grouped_txt_info:
+        original_texts += ' '
+        original_texts += info[4]
         original_texts += '\n'
     
     print('the original texts are below')
@@ -281,46 +451,49 @@ def lambda_handler(event, context):
 
 
 
-
-
+    start_time = time.time()
     # translate them
     translated_texts = translate_with_gpt(original_texts)
+    end_time = time.time()
     print(translated_texts)
+    print("the time it took to translate all is" + str(end_time-start_time))
 
-    sentences = translated_texts.split("\n##")
+    sentences = translated_texts.split("\n")
+    sentences[0] = sentences[0][2:] # remove the first '##'
+
+
+
+    # update grouped_txt_info
+    for index_outer, info in enumerate(grouped_txt_info):
+        grouped_txt_info[index_outer][4] = sentences[index_outer]
+
+    print("this one below should be a text line storage (translated)")
+    print(*grouped_txt_info) # contains each geo info and translated texts
 
 
 
 
 
-    # update txt_line_storage
-    for index_outer, info in enumerate(txt_line_storage):
-        for index_inner, sentence in enumerate(sentences):
-            translated_text = sentence
-            if (index_outer == index_inner):
-                if (index_outer == 0):
-                    translated_text = translated_text[2:]
-                txt_line_storage[index_outer][0] = translated_text
-    translated_txt_line_storage = txt_line_storage
 
-    print(*translated_txt_line_storage) # contains translated texts and the number of LINEs
+
+
 
 
     text_info_list_index = 0
     translated_text_info_list = []
-
-    #TODO allocate the translated texts into each LINE block
+     
     current_line = 0
-    for chunk in translated_txt_line_storage:
-        translated_complete_txt = chunk[0]
-        num_of_line = chunk[1]
-
-        # Initialize a list to store the selected dictionaries(containing one relational block)
-        selected_dicts = []
+    for chunk in grouped_txt_info:
+        translated_txt = chunk[4]
+        #num_of_line = chunk[5]
 
         line_ratios = []
 
-        # Loop through the list and select dictionaries within the specified range
+
+        for width in chunk[2]:
+            line_ratios.append(width)
+
+        """  # Loop through the list and select dictionaries within the specified range
         for position in range(current_line, current_line+num_of_line):
             if 0 <= position < len(text_info_list):
                 selected_dicts.append(text_info_list[position])
@@ -335,16 +508,26 @@ def lambda_handler(event, context):
         current_line += num_of_line
 
         print("the selected dicts is")
-        print(*selected_dicts)
+        print(*selected_dicts) """
 
         print("the line ratio is")
         print(*line_ratios)
     
-        splitted_result = split_sentence_by_characters(translated_complete_txt, line_ratios)
+        splitted_result = split_sentence_by_characters(translated_txt, line_ratios)
         
+        print("splitted result is")
+        print(*splitted_result)
+        for index, splitted_text in enumerate(splitted_result):  # 3D list [x, y, width, height, text, the number of consisted lines]
 
-        for splitted_text in splitted_result:
-            line_info = {'Text': splitted_text, 'BoundingBox': text_info_list[text_info_list_index]['BoundingBox']}
+            BoudningBoxDict = {
+                'Width': chunk[2][index],
+                'Height': chunk[3][index],
+                'Left': chunk[0][index],
+                'Top': chunk[1][index]
+            }
+
+
+            line_info = {'Text': splitted_text, 'BoundingBox': BoudningBoxDict}
             translated_text_info_list.append(line_info)
             text_info_list_index += 1
 
